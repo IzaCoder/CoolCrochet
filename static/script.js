1aad2e7acc7130f6f22692e6e8b8ff4d30105d3e
IzaCoder <85595031+IzaCoder@users.noreply.github.com>
2021-09-01 22:57:29 -0400

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,12 @@
+var pageNum = 1
+
+function showPattern() {
+    const patternFile = document.getElementById("pattern_file")
+    const patternText = document.getElementById("pattern_text")
+    
+    if (patternFile.files) {
+        alert("File!") // dummy code, to be changed later
+    } else if (patternText.value) {
+        alert("Text!") // dommy code, to be changed later
+    }
+}
